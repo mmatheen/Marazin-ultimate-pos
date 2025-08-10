f756698e05f058ce0b3736b9df3c88c5ca42ebd3
Mohamed Ahshan <ahshan.marazin0056@gmail.com>
2025-08-10 17:59:04 +0530

    Update backup.php

--- config/backup.php
+++ config/backup.php
@@ -15,7 +15,7 @@ return [
                  * The list of directories and files that will be included in the backup.
                  */
                 'include' => [
-                    base_path(),
+                    // base_path(),
                 ],
 
                 /*
@@ -24,8 +24,8 @@ return [
                  * Directories used by the backup process will automatically be excluded.
                  */
                 'exclude' => [
-                    base_path('vendor'),
-                    base_path('node_modules'),
+                    // base_path('vendor'),
+                    // base_path('node_modules'),
                 ],
 
                 /*
